f7f1dabc26b66c4aea6e29b8dafb60a33a312f31
zearkiatos <caprilespe@outlook.com>
2022-05-14 22:23:03 -0400

    Creating a components

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import LoginForm from "./src/components/LoginForm";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World! 👋 🌎</Text>
+      <LoginForm />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
