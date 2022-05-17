2bf1e6d39a0de4c34a48c4aafa7dee9aee3e65eb
zearkiatos <caprilespe@outlook.com>
2022-05-16 20:59:00 -0400

    Installing and configuration React Navigation

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
+import "react-native-gesture-handler";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 import Greeting from "./src/components/Greeting";
 import LoginForm from "./src/components/LoginForm";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World! 👋 🌎</Text>
-      <LoginForm />
-      <Greeting firstname="Pedro" lastname="Capriles" />
-      <Greeting firstname="Luis" lastname="Perez" />
-      <Greeting firstname="Maria" lastname="Rojas" />
-      <Greeting />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Hello World! 👋 🌎</Text>
+        <LoginForm />
+        <Greeting firstname="Pedro" lastname="Capriles" />
+        <Greeting firstname="Luis" lastname="Perez" />
+        <Greeting firstname="Maria" lastname="Rojas" />
+        <Greeting />
+      </View>
+    </NavigationContainer>
   );
 }
 
