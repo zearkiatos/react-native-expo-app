efb3af631f149d563d42ebab35ac2051cc06c276
zearkiatos <caprilespe@outlook.com>
2022-05-18 20:13:51 -0400

    Navigation stacks

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
 import "react-native-gesture-handler";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import Greeting from "./src/components/Greeting";
-import LoginForm from "./src/components/LoginForm";
+import NavigationStack from "./src/navigation/NavigationStack";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Hello World! 👋 🌎</Text>
-        <LoginForm />
-        <Greeting firstname="Pedro" lastname="Capriles" />
-        <Greeting firstname="Luis" lastname="Perez" />
-        <Greeting firstname="Maria" lastname="Rojas" />
-        <Greeting />
-      </View>
+      <NavigationStack />
     </NavigationContainer>
   );
 }
