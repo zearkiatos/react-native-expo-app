4f9ec6244609dc0be0b6066cfb625a31d3f422d4
zearkiatos <caprilespe@outlook.com>
2022-05-21 11:06:50 -0400

    Drawer navigation and next steps

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import "react-native-gesture-handler";
 import React from "react";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import NavigationTab from "./src/navigation/NavigationTab";
+import NavigationDrawer from "./src/navigation/NavigationDrawer";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <NavigationTab />
+      <NavigationDrawer />
     </NavigationContainer>
   );
 }
